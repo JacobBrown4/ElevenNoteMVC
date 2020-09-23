a138f03abe0e8a66f2fc645946b14f6cd384f887
JacobBrown4 <68622976+JacobBrown4@users.noreply.github.com>
2020-09-23 14:17:21 -0400

    Services

--- ElevenNote.Data/Note.cs
+++ ElevenNote.Data/Note.cs
@@ -12,13 +12,13 @@ namespace ElevenNote.Data
         [Key]
         public int NoteId { get; set; }
         [Required]
-        public Guid guid { get; set; }
+        public Guid OwnerId { get; set; }
         [Required]
         public string Title { get; set; }
         [Required]
         public string Content { get; set; }
         [Required]
-        public DateTimeOffset DateTimeOffset { get; set; }
+        public DateTimeOffset CreatedUtc { get; set; }
         public DateTimeOffset? ModifiedUtc { get; set; }
     }
 }
